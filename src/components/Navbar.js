56c824149af08107d073049bc0600f388772f0f0
nandji3@gmail.com <115684770+nandji3@users.noreply.github.com>
2023-05-11 15:51:08 +0530

    Again used React-Router after TextUtils App deployeed on  github

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,8 +9,8 @@ export default function Navbar(props) {
 
             <div className="container-fluid">
 
-                {/* <Link className="navbar-brand" to="/">{props.title}</Link> */}
-                <a className="navbar-brand" href="/">{props.title}</a>
+                <Link className="navbar-brand" to="/">{props.title}</Link>
+                {/* <a className="navbar-brand" href="/">{props.title}</a> */}
 
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -21,13 +21,13 @@ export default function Navbar(props) {
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
 
                         <li className="nav-item">
-                            {/* <Link className="nav-link active" aria-current="page" to="#">Home</Link> */}
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                            {/* <a className="nav-link active" aria-current="page" href="#">Home</a> */}
                         </li>
 
                         <li className="nav-item">
-                            {/* <Link className="nav-link" to="/about">{props.aboutText}</Link> */}
-                            <a className="nav-link" href="/about">{props.aboutText}</a>
+                            <Link className="nav-link" to="/about">{props.aboutText}</Link>
+                            {/* <a className="nav-link" href="/about">{props.aboutText}</a> */}
                         </li>
                     </ul>
 
